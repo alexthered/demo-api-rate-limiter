c2ecc59a0accd662fcbbb7c52842998311e2462a
Hiep Doan <hiep.dv179@gmail.com>
2019-03-16 21:43:44 +0100

    🎉 Setup the project

---
+++ src/main/java/com/alexthered/apiratelimiter/demo/ApiRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.alexthered.apiratelimiter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRateLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexthered/apiratelimiter/demo/ApiRateLimiterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexthered.apiratelimiter.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiRateLimiterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
